3182902bce085af2f2e1b5d105208481048b760e
“jiazehua” <divgg@foxmail.com>
2023-11-21 11:49:18 +0800

    新版本

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,3 @@ import VideoList from './components/VideoList.vue';
   <VideoList />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  padding: 10px;
-}
-</style>
--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,491 +1,218 @@
-<script setup>
-import { onMounted, reactive, ref, toRaw, watch } from 'vue';
-import axios from 'axios';
-import { ElMessage } from 'element-plus';
-import localforage from 'localforage';
-import Darkmode from 'darkmode-js';
-import { Delete, Edit, Search, Share, Upload } from '@element-plus/icons-vue';
-
-/**
- * @Author: jiazehua
- * @Description: 定义参数
- * @param {*}
- * @return {*}
- */
-
-defineProps({
-  msg: String
-});
-
-/**
- * @Author: jiazehua
- * @Description: 定义响应式数据
- * @param {*}
- * @return {*}
- */
-const data = reactive({
-  bv: '',
-  videoList: [],
-  dataInterval: '',
-  second: 8,
-  fansCount: 0,
-  fansChangeCount: 0,
-  uid: ''
-});
-
-/**
- * @Author: jiazehua
- * @Description: 监听 视频列表
- * @param {*}
- * @return {*}
- */
-watch(
-  () => data.videoList,
-  val => {
-    setCache(val);
-    if (val?.length) {
-      let _title = val[0].title;
-      document.getElementsByTagName('title')[0].innerText = _title;
-    }
-  },
-  { deep: true }
-);
-
-/**
- * @Author: jiazehua
- * @Description: 监听 计时器 刷新数据
- * @param {*}
- * @return {*}
- */
-watch(
-  () => data.second,
-  val => {
-    loopGetData();
-  }
-);
-
-/**
- * 监听粉丝数量变化
- */
-watch(
-  () => data.fansCount,
-  (val, oldVal) => {
-    if (oldVal) {
-      data.fansChangeCount = val - oldVal;
-    }
-  }
-);
-
-/**
- * 监听粉丝数量变化
- */
-watch(
-  () => data.uid,
-  (val, oldVal) => {
-    // 判断 val 是否为数字
-    if (val && !isNaN(val)) {
-      // 存入 sessionStorage
-      console.log('val :>> ', val);
-      getUpInfo();
-    }
-  }
-);
-
-/**
- * @Author: jiazehua
- * @Description: 页面初始化方法
- * @param {*}
- * @return {*}
- */
-onMounted(() => {
-  const bvListString = getUrlParam('bv');
-  if (bvListString) {
-    let bvList = bvListString.split(',');
-    let newBvList = [];
-    bvList.forEach(bv => {
-      newBvList.push({
-        bv
-      });
-    });
-    data.videoList = newBvList;
-    setCache(newBvList);
-  } else {
-    getCache();
-  }
-  setTimeout(() => {
-    getData();
-    getUpInfo();
-  }, 500);
-  loopGetData();
-  new Darkmode().showWidget();
-});
-
-function setCache(val) {
-  const _json = JSON.parse(JSON.stringify(val));
-  localforage.setItem('__bvlist', _json);
-}
-
-/**
- * 获取UP主信息，粉丝数等
- */
-function getUpInfo() {
-  // 请求 URL: https://api.bilibili.com/x/web-interface/search/all/v2?__refresh__=true&_extra=&context=&page=1&page_size=42&order=&duration=&from_source=&from_spmid=333.337&platform=pc&highlight=1&single_column=0&keyword=%E7%A9%BF%E5%B1%B1%E8%B4%BE%E8%AF%B4%E4%BA%86%E5%95%A5&preload=true&com2co=true
-  if (!data.uid) return;
-  console.log('data.uid :>> ', data.uid);
-  if (isNaN(data.uid)) return;
-  // data.uid去除空格
-  data.uid = data.uid.replace(/\s*/g, '');
-  axios.get('/api/x/relation/stat?vmid=' + data.uid).then(res => {
-    data.fansCount = 0;
-    if (res.status !== 200) return;
-    console.log('res.data :>> ', res.data);
-    const { follower } = res.data.data;
-    console.log('follower :>> ', follower);
-    data.fansCount = follower;
-  });
-}
-
-/**
- * @Author: jiazehua
- * @Description: 获取页面初始化的url参数
- * @param {*}
- * @return {*}
- */
-function getCache() {
-  localforage
-    .getItem('__bvlist')
-    .then(res => {
-      if (res?.length) {
-        data.videoList = res;
-      }
-    })
-    .catch(e => {
-      console.log('e :>> ', e);
-    });
-}
-
-/**
- * @Author: jiazehua
- * @Description: 添加视频
- * @param {*}
- * @return {*}
- */
-function addVideoHandler() {
-  if (!data.bv) return;
-  let bvList = [];
-  data.videoList.forEach(element => {
-    bvList.push(element.bv);
-  });
-  if (bvList.includes(data.bv)) {
-    ElMessage({
-      message: '请勿重复添加',
-      type: 'error'
-    });
-    return;
-  } else {
-    data.videoList.push({
-      bv: data.bv
-    });
-    getData();
-  }
-  data.bv = '';
-}
-
-/**
- * @Author: jiazehua
- * @Description: 获取视频数据
- * @param {*}
- * @return {*}
- */
-function getData() {
-  data.videoList.forEach(element => {
-    axios
-      .get('/api/x/web-interface/view?bvid=' + element.bv)
-      .then(res => {
-        if (res.status !== 200) return;
-        const data = res.data.data;
-        // const {cid, bvid} = data
-        return data;
-      })
-      .then(res => {
-        if (!res) return;
-        let { cid, bvid, title, stat } = res;
-        axios.get(`/api/x/player/online/total?bvid=${bvid}&cid=${cid}`).then(moreInfo => {
-          const data = moreInfo.data.data;
-          const targetInfo = {
-            view: stat.view,
-            online: data?.total || '',
-            like: stat.like,
-            coin: stat.coin,
-            reply: stat.reply,
-            danmaku: stat.danmaku
-          };
-          element.view = targetInfo.view;
-          element.title = title;
-          element.online = targetInfo.online;
-          element.like = targetInfo.like;
-          element.coin = targetInfo.coin;
-          element.reply = targetInfo.reply;
-          element.danmaku = targetInfo.danmaku;
-        });
-      });
-  });
-}
-
-/**
- * @Author: jiazehua
- * @Description: 移除视频
- * @param {*} videoItem
- * @return {*}
- */
-function removeVideo(videoItem) {
-  let newList = [];
-  data.videoList.forEach(element => {
-    if (element.bv !== videoItem.bv) {
-      newList.push(element);
-    }
-  });
-  data.videoList = newList;
-  ElMessage({
-    message: '移除成功',
-    type: 'success'
-  });
-}
-
-/**
- * @Author: jiazehua
- * @Description: 复制地址方法
- * @param {*} id
- * @return {*}
- */
-function copyUrl(id) {
-  $('body').after("<input id='copyVal'></input>");
-  var text = id;
-  var input = document.getElementById('copyVal');
-  input.value = text;
-  input.select();
-  input.setSelectionRange(0, input.value.length);
-  document.execCommand('copy');
-  $('#copyVal').remove();
-}
-
-/**
- * @Author: jiazehua
- * @Description: 千位分割 template调用
- * @param {*}
- * @return {*}
- */
-
-function commafy(num) {
-  return num ? String(num).replace(/(\d)(?=(\d{3})+$)/g, '$1,') : 0;
-}
-
-/**
- * @Author: jiazehua
- * @Description: 获取URL参数
- * @param {*} name
- * @return {*}
- */
-function getUrlParam(name) {
-  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)'); // 构造一个含有目标参数的正则表达式对象
-  var r = window.location.search.substr(1).match(reg); // 匹配目标参数
-  if (r != null) return unescape(r[2]);
-  return null; // 返回参数值
-}
-
-/**
- * @Author: jiazehua
- * @Description: 定时刷新数据
- * @param {*}
- * @return {*}
- */
-function loopGetData() {
-  if (data.dataInterval) {
-    clearInterval(data.dataInterval);
-  }
-  data.dataInterval = setInterval(() => {
-    data.fansChangeCount = 0;
-    getData();
-    getUpInfo();
-  }, 1000 * data.second);
-}
-
-/**
- * @Author: jiazehua
- * @Description: 分享按钮
- * @param {*}
- * @return {*}
- */
-function share() {
-  let url = 'http://' + window.document.domain;
-  let params = data.videoList?.length ? '?bv=' : '';
-  let paramsArray = [];
-  if (data?.videoList?.length) {
-    data.videoList.forEach(element => {
-      paramsArray.push(element.bv);
-    });
-  }
-  url = url + params + paramsArray.join();
-
-  copyUrl(url);
-  ElMessage({
-    message: '地址已复制，分享给别人吧！',
-    type: 'success'
-  });
-}
-</script>
-
 <template>
-  <div style="max-width: 1000px;margin: 0 auto">
-    <div style="margin-bottom: 5px">
-      <div style="display: flex">
-        <div style="margin-right: 10px">
-          <el-input
-            clearable
-            v-model="data.uid"
-            placeholder="输入UP昵称"
-          ></el-input>
-        </div>
-        <div
-          style="width: 140px;margin-right: 10px"
-          v-if="data.fansCount"
-        >
-          <el-badge
-            v-if="data.fansChangeCount"
-            :value="(data.fansChangeCount > 0 ? '+' : '' )+ data.fansChangeCount"
-            :type="data.fansChangeCount > 0 ? 'danger' : 'success'"
-            class="item"
-          >
-            <p style="line-height: 32px; margin:0; padding-right:5px">
-              粉丝数
-              <span
-                :class="data.fansChangeCount ?( data.fansChangeCount > 0 ? 'text-danger' : 'text-success') : 'text-default'"
-                style="font-weight: 900"
-              >{{data.fansCount}}</span>
-            </p>
-          </el-badge>
-          <p
-            v-else
-            style="line-height: 32px; margin:0; padding-right:5px"
-          >
-            粉丝数
-            <span
-              :class="data.fansChangeCount ?( data.fansChangeCount > 0 ? 'text-danger' : 'text-success') : 'text-default'"
-              style="font-weight: 900"
-            >{{data.fansCount}}</span>
-          </p>
-        </div>
+  <div class="i-wrapper">
+    <el-table
+      :data="data.tableData"
+      style="width: 100%"
+      border
+      stripe
+    >
+      <el-table-column
+        fixed
+        prop="title"
+        label="标题"
+        width="180"
+      ></el-table-column>
+      <el-table-column
+        prop="view"
+        label="播放"
+        width="90"
+      ></el-table-column>
+      <el-table-column
+        prop="online"
+        label="在线"
+        width="80"
+      ></el-table-column>
 
-        <el-button
-          @click="share"
-          type="primary"
-        >分享</el-button>
-      </div>
-    </div>
-
-    <!-- <el-button
-            @click="share"
-            type="primary"
-    >分享</el-button>-->
-    <div style="margin-bottom: 10px">
-      <el-input
-        clearable
-        v-model="data.bv"
-        placeholder="输入BV号"
+      <el-table-column
+        prop="like"
+        label="点赞"
+        width="80"
+      ></el-table-column>
+      <el-table-column
+        prop="coin"
+        label="投币"
+        width="80"
+      ></el-table-column>
+      <el-table-column
+        prop="reply"
+        label="评论"
+        width="80"
+      ></el-table-column>
+      <el-table-column
+        prop="danmaku"
+        label="弹幕"
+        width="80"
+      ></el-table-column>
+      <el-table-column
+        label="操作"
+        width="120"
       >
-        <!-- <template #prepend>https://www.bilibili.com/video/</template> -->
-        <template #append>
+        <template #default>
+          <!-- 获取参数 定义remove -->
           <el-button
-            @click="addVideoHandler"
+            link
             type="primary"
+            size="small"
+            @click="remove(params)"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 添加按钮 -->
+    <div class="mt-4">
+      <el-input
+        v-model="data.bv"
+        placeholder="bv号"
+        class="input-with-select"
+      >
+        <template #append>
+          <!-- 蓝色 -->
+          <el-button
+            type="primary"
+            @click="add"
           >添加</el-button>
         </template>
       </el-input>
     </div>
-    <div
-      v-for="(item, index) in data.videoList"
-      :key="index + '_video'"
-      class="mb-2 video-box"
-    >
-      <el-descriptions
-        :title="item.title"
-        border
-        style="width: 100%"
-        :column="2"
-      >
-        <template #extra>
-          <el-popconfirm
-            title="确认移除观察列表？"
-            @confirm="removeVideo(item)"
-          >
-            <template #reference>
-              <el-button
-                type="danger"
-                size="small"
-                :icon="Delete"
-              />
-
-              <!-- <el-button type="danger"  :icon="Delete" >移除</el-button> -->
-            </template>
-          </el-popconfirm>
-        </template>
-        <el-descriptions-item label="播放">
-          <span style="color: #e47470">{{commafy(item.view)}}</span>
-        </el-descriptions-item>
-        <el-descriptions-item label="在线">
-          <span style="color: #e47470">{{commafy(item.online)}}</span>
-        </el-descriptions-item>
-        <el-descriptions-item label="点赞">{{commafy(item.like)}}</el-descriptions-item>
-        <el-descriptions-item label="投币">{{commafy(item.coin)}}</el-descriptions-item>
-        <el-descriptions-item label="评论">{{commafy(item.reply)}}</el-descriptions-item>
-        <el-descriptions-item label="弹幕">{{commafy(item.danmaku)}}</el-descriptions-item>
-      </el-descriptions>
-    </div>
   </div>
 </template>
+<script setup>
+import { ref, reactive, onMounted, watch } from 'vue';
+let data = reactive({
+  bv: '',
+  bvList: [],
+  uid: '',
+  tableData: [
+    // {
+    //   title: '妻子：他太看重血缘了，禽兽不如！结婚十六年，结果3个孩子都不是亲生的',
+    //   view: '100.1万',
+    //   online: '1000+',
+    //   like: '9999',
+    //   coin: '9999',
+    //   reply: '9999',
+    //   danmaku: '9999'
+    // },
+    // {
+    //   title: '妻子：他太看重血缘了，禽兽不如！结婚十六年，结果3个孩子都不是亲生的',
+    //   view: '999999',
+    //   online: '1000+'
+    // },
+    // {
+    //   title: '妻子：他太看重血缘了，禽兽不如！结婚十六年，结果3个孩子都不是亲生的',
+    //   view: '999999',
+    //   online: '1000+'
+    // },
+    // {
+    //   title: '妻子：他太看重血缘了，禽兽不如！结婚十六年，结果3个孩子都不是亲生的',
+    //   view: '999999',
+    //   online: '1000+'
+    // }
+  ]
+});
 
+// 深度检测data.bvList的变化
+watch(
+  () => data.bvList,
+  bvList => {
+    // 存入本地存储
+    console.log('bvList :>> ', bvList);
+  },
+  { deep: true }
+);
 
-<style lang="scss">
-.mb-2 {
-  margin-bottom: 1rem;
+const setLocalStorage = (key, data) => {
+  // 先把data转为字符串
+  const dataStr = JSON.stringify(data);
+  // 再存入本地存储
+  window.localStorage.setItem(key, dataStr);
+};
+
+const getLocalStorage = key => {
+  // 先获取本地存储的字符串
+  const dataStr = window.localStorage.getItem(key);
+  // 再把字符串转为对象
+  const dataObj = JSON.parse(dataStr);
+  // 返回对象
+  return dataObj;
+};
+
+// 从url params取uid
+const getUid = () => {
+  // 获取url参数
+  const params = new URLSearchParams(window.location.search);
+  // 获取uid
+  const uid = params.get('uid');
+  // 返回uid
+  return uid;
+};
+
+// 从url params 取bvList
+const getBvList = () => {
+  // 获取url参数
+  const params = new URLSearchParams(window.location.search);
+  // 获取bvList
+  const bvList = params.get('bvList');
+  // 返回bvList
+  // 如果有bvList则把这个逗号分割的字符串转为数组
+  if (bvList) {
+    return bvList.split(',');
+  } else {
+    return [];
+  }
+};
+
+onMounted(() => {
+  // 启动循环获取数据，根据bvList生成tableData
+  getDataList();
+  setTimeout(() => {
+    getDataList();
+  }, 1000);
+});
+
+const getDataList = () => {
+  if (data.bvList.length === 0) return;
+  data.bvList.forEach(element => {
+    console.log('element :>> ', element);
+  });
+};
+
+// 删除行
+const remove = params => {
+  console.log('remove');
+  // 获取参数
+  const { row, rowIndex } = params;
+  console.log(row, rowIndex);
+};
+
+// 添加行
+const add = () => {
+  if (!data?.bv) return;
+  // 检查bv是否已经存在于bvList
+  const isExist = data.bvList.some(item => item === data.bv);
+  if (isExist) {
+    alert('已经存在');
+    return;
+  }
+  // 添加bv到bvList
+  data.bvList.push(data.bv);
+  console.log('data.bvList :>> ', data.bvList);
+  data.bv = '';
+};
+</script>
+<style lang="scss" >
+// css大屏幕兼容性
+@media screen and (min-width: 1024px) {
+  .i-wrapper {
+    box-sizing: border-box;
+    width: 430px;
+    border: 1px solid #ddd;
+    min-height: 100vh;
+    margin: 0 auto;
+    background-color: #fff;
+  }
 }
-.mb-4 {
-  margin-bottom: 2rem;
-}
-.el-descriptions__title {
-  overflow: hidden;
-  max-width: 85% !important;
-  word-break: keep-all; /* 不换行 */
-  white-space: nowrap; /* 不换行 */
-  overflow: hidden; /* 内容超出宽度时隐藏超出部分的内容 */
-  text-overflow: ellipsis; /* 当对象内文本溢出时显示省略标记(...) ；需与overflow:hidden;一起使用。*/
-}
-td.is-bordered-content {
-  font-size: 1rem !important;
-  font-weight: 900 !important;
-}
-.video-box {
-  background: #fff;
-  padding: 5px 10px 20px 10px;
-  border-radius: 5px;
-}
-html,
 body {
-  background-color: #f5f5f5 !important;
-  min-height: 100vh;
-  padding: 0 !important;
   margin: 0 !important;
-}
-.el-descriptions__header {
-  margin-bottom: 6px !important;
-}
-.text-danger {
-  color: #e47470;
-}
-.text-success {
-  color: #7ec050;
-}
-.text-default {
-  color: #000;
+  background: #252525;
 }
 </style>
